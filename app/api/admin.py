fc5c630e25137d29b3e6f3fe94e55a34a73eb77c
Antoine Grandry <antoine.grandry@hotmail.fr>
2020-10-26 17:16:37 +0100

    add albums editing field in ArtistAdmin

--- app/api/admin.py
+++ app/api/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from app.api import models
 
 
+class HasBeenCreatedByInline(admin.TabularInline):
+    model = models.Album.artists.through
+
 class ArtistAdmin(admin.ModelAdmin):
     model = models.Artist
     readonly_fields = ('uuid',)
+    inlines = [
+        HasBeenCreatedByInline,
+    ]
     list_display = ['name', 'uuid']
     search_fields = ['name', 'uuid', 'albums__title']
 
